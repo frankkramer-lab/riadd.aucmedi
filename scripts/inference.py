72e5999189ffe3ab0bb1416a5499a127f6e6f795
muellerdo <domey038@live.de>
2021-02-20 19:08:22 +0100

    Implemented inference for RIADD evaluation set (computing detector and classifier predictions)

--- scripts/inference.py
+++ scripts/inference.py
@@ -46,23 +46,10 @@ processes = 8
 batch_queue_size = 16
 threads = 16
 
-# Define architectures which should be processed
-architectures = ["DenseNet201", "ResNet152", "InceptionV3"]
-
-# Define input shape
-input_shape = (224, 224)
-
-#-----------------------------------------------------#
-#          AUCMEDI Classifier Setup for RIADD         #
-#-----------------------------------------------------#
-# Initialize input data reader
-cols = ["DR", "ARMD", "MH", "DN", "MYA", "BRVO", "TSLN", "ERM", "LS", "MS", "CSR",
-        "ODC", "CRVO", "TV", "AH", "ODP", "ODE", "ST", "AION", "PT", "RT", "RS",
-        "CRS", "EDN", "RPEC", "MHL", "RP", "OTHER"]
-ds = input_interface(interface="directory", path_imagedir=path_riadd,
-                     path_data=None, training=False)
-(index_list, class_ohe, nclasses, class_names, image_format) = ds
-nclasses = len(cols)
+# Define label columns
+cols = ["Disease_Risk", "DR", "ARMD", "MH", "DN", "MYA", "BRVO", "TSLN", "ERM",
+        "LS", "MS", "CSR", "ODC", "CRVO", "TV", "AH", "ODP", "ODE", "ST",
+        "AION", "PT", "RT", "RS", "CRS", "EDN", "RPEC", "MHL", "RP", "OTHER"]
 
 # Create result directory
 path_res = os.path.join("preds")
@@ -70,17 +57,41 @@ if not os.path.exists(path_res) : os.mkdir(path_res)
 # Obtain model directory
 path_models = os.path.join("models")
 
+#-----------------------------------------------------#
+#       General AUCMEDI Inference Pipeline Setup      #
+#-----------------------------------------------------#
+# Initialize input data reader
+ds = input_interface(interface="directory", path_imagedir=path_riadd,
+                     path_data=None, training=False)
+(index_list, _, _, _, image_format) = ds
+
 # Define Subfunctions
 sf_list = [Padding(mode="square"), Retinal_Crop()]
+
+# Initialize Image Augmentation
+aug = Image_Augmentation(flip=True, rotate=True, brightness=True, contrast=True,
+                         saturation=False, hue=False, scale=False, crop=False,
+                         grid_distortion=False, compression=False, gamma=False,
+                         gaussian_noise=False, gaussian_blur=False,
+                         downscaling=False, elastic_transform=False)
+
+#-----------------------------------------------------#
+#            AUCMEDI Classifier Inference             #
+#-----------------------------------------------------#
+# Define number of classes
+nclasses = len(cols[1:])
 
 # Set activation output to sigmoid for multi-label classification
 activation_output = "sigmoid"
 
-#-----------------------------------------------------#
-#        AUCMEDI Classifier Inference for RIADD       #
-#-----------------------------------------------------#
-for arch in architectures:
-    path_arch = os.path.join(path_models, arch)
+# Iterate over all classifier architectures
+for model_subdir in os.listdir(path_models):
+    # Skip all non classifier model subdirs
+    if not model_subdir.startswith("classifier_") : continue
+    # Identify architecture
+    arch = model_subdir.split("_")[1]
+    path_arch = os.path.join(path_models, model_subdir)
+
     # Iterate over each fold of the CV
     for i in range(0, k_fold):
         # Initialize model
@@ -94,6 +105,9 @@ for arch in architectures:
 
         # Obtain standardization mode for current architecture
         sf_standardize = supported_standardize_mode[arch]
+
+        # Define input shape
+        input_shape = model.input_shape[:-1]
 
         # Initialize Data Generator for prediction
         pred_gen = DataGenerator(index_list, path_riadd, labels=None,
@@ -109,35 +123,120 @@ for arch in architectures:
         path_cv_model = os.path.join(path_arch, "cv_" + str(i) + ".model.best.hdf5")
         if os.path.exists(path_cv_model) : model.load(path_cv_model)
         else:
-            print("Skipping model:", arch, str(i))
+            print("Skipping model:", model_subdir, arch, str(i))
 
         # Use fitted model for predictions
         preds = model.predict(pred_gen)
         # Create prediction dataset
         df_index = pd.DataFrame(data={"ID": index_list})
-        df_pd = pd.DataFrame(data=preds, columns=[s for s in cols])
+        df_pd = pd.DataFrame(data=preds, columns=[s for s in cols[1:]])
         df_merged = pd.concat([df_index, df_pd], axis=1, sort=False)
         df_merged.sort_values(by=["ID"], inplace=True)
         # Store predictions to disk
-        df_merged.to_csv(os.path.join(path_res, arch + "." + "cv_" + str(i) + \
-                                      ".labels.simple.predictions.csv"),
+        df_merged.to_csv(os.path.join(path_res, "classifier." + arch + "." + \
+                                      "cv_" + str(i) + ".inference.simple.csv"),
                          index=False)
 
-        # Apply Inference Augmenting
+        # Run Inference Augmenting
         preds = predict_augmenting(model, index_list, path_riadd, n_cycles=20,
-                                   img_aug=None, aggregate="mean",
+                                   img_aug=aug, aggregate="mean",
                                    image_format=image_format, batch_size=64,
                                    resize=input_shape, grayscale=False,
                                    subfunctions=sf_list, seed=None,
-                                   standardize_mode=sf_standardize, workers=threads)
+                                   standardize_mode=sf_standardize,
+                                   workers=threads)
         # Create prediction dataset
         df_index = pd.DataFrame(data={"ID": index_list})
-        df_pd = pd.DataFrame(data=preds, columns=[s for s in cols])
+        df_pd = pd.DataFrame(data=preds, columns=[s for s in cols[1:]])
         df_merged = pd.concat([df_index, df_pd], axis=1, sort=False)
         df_merged.sort_values(by=["ID"], inplace=True)
         # Store predictions to disk
-        df_merged.to_csv(os.path.join(path_res, arch + "." + "cv_" + str(i) + \
-                                      ".labels.augmenting_mean.predictions.csv"),
+        df_merged.to_csv(os.path.join(path_res, "classifier." + arch + "." + \
+                                      "cv_" + str(i) + ".inference.augmenting.csv"),
+                         index=False)
+
+        # Garbage collection
+        del pred_gen
+        del model
+
+#-----------------------------------------------------#
+#              AUCMEDI Detector Inference             #
+#-----------------------------------------------------#
+# Define number of classes
+nclasses = 2
+
+# Set activation output to softmax for binary classification
+activation_output = "softmax"
+
+# Iterate over all detector architectures
+for model_subdir in os.listdir(path_models):
+    # Skip all non detector model subdirs
+    if not model_subdir.startswith("detector_") : continue
+    # Identify architecture
+    arch = model_subdir.split("_")[1]
+    path_arch = os.path.join(path_models, model_subdir)
+
+    # Iterate over each fold of the CV
+    for i in range(0, k_fold):
+        # Initialize model
+        model = Neural_Network(nclasses, channels=3, architecture=arch,
+                               workers=processes,
+                               batch_queue_size=batch_queue_size,
+                               activation_output=activation_output,
+                               loss="categorical_crossentropy",
+                               metrics=["categorical_accuracy", AUC(100)],
+                               pretrained_weights=True, multiprocessing=True)
+
+        # Obtain standardization mode for current architecture
+        sf_standardize = supported_standardize_mode[arch]
+
+        # Define input shape
+        input_shape = model.input_shape[:-1]
+
+        # Initialize Data Generator for prediction
+        pred_gen = DataGenerator(index_list, path_riadd, labels=None,
+                                 batch_size=64, img_aug=None,
+                                 subfunctions=sf_list,
+                                 standardize_mode=sf_standardize,
+                                 shuffle=False, resize=input_shape,
+                                 grayscale=False, prepare_images=False,
+                                 sample_weights=None, seed=None,
+                                 image_format=image_format, workers=threads)
+
+        # Load best model
+        path_cv_model = os.path.join(path_arch, "cv_" + str(i) + ".model.best.hdf5")
+        if os.path.exists(path_cv_model) : model.load(path_cv_model)
+        else:
+            print("Skipping model:", model_subdir, arch, str(i))
+
+        # Use fitted model for predictions
+        preds = model.predict(pred_gen)
+        # Create prediction dataset
+        df_index = pd.DataFrame(data={"ID": index_list})
+        df_pd = pd.DataFrame(data={cols[0]: preds[:, 1]})
+        df_merged = pd.concat([df_index, df_pd], axis=1, sort=False)
+        df_merged.sort_values(by=["ID"], inplace=True)
+        # Store predictions to disk
+        df_merged.to_csv(os.path.join(path_res, "detector." + arch + "." + \
+                                      "cv_" + str(i) + ".inference.simple.csv"),
+                         index=False)
+
+        # Run Inference Augmenting
+        preds = predict_augmenting(model, index_list, path_images, n_cycles=20,
+                                   img_aug=aug, aggregate="mean",
+                                   image_format=image_format, batch_size=64,
+                                   resize=input_shape, grayscale=False,
+                                   subfunctions=sf_list, seed=None,
+                                   standardize_mode=sf_standardize,
+                                   workers=threads)
+        # Create prediction dataset
+        df_index = pd.DataFrame(data={"ID": index_list})
+        df_pd = pd.DataFrame(data={cols[0]: preds[:, 1]})
+        df_merged = pd.concat([df_index, df_pd], axis=1, sort=False)
+        df_merged.sort_values(by=["ID"], inplace=True)
+        # Store predictions to disk
+        df_merged.to_csv(os.path.join(path_res, "detector." + arch + "." + \
+                                      "cv_" + str(i) + ".inference.augmenting.csv"),
                          index=False)
 
         # Garbage collection
